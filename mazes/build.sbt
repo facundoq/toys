f2f29fd5c87f56d08fa9a92787c698b33b358825
Facundo Quiroga <facundoq@gmail.com>
2017-02-10 00:52:25 -0300

    coding 'generate'

--- mazes/build.sbt
+++ mazes/build.sbt
@@ -2,6 +2,6 @@ enablePlugins(ScalaJSPlugin)
 
 name := "Mazes"
 
-scalaVersion := "2.11"
+scalaVersion := "2.11.6"
 
 libraryDependencies += "org.scala-js" %%% "scalajs-dom" % "0.9.1"
--- mazes/src/main/scala/mazes/webapp/DungeonGenerator.scala
+++ mazes/src/main/scala/mazes/webapp/DungeonGenerator.scala
@@ -2,6 +2,7 @@ package mazes.webapp
 
 import scala.util.Random
 import mazes.webapp.Direction.DirectionVal
+import scala.util.control.Breaks._
 
 class Size(var rows: Int, var cols: Int) {
 
@@ -11,6 +12,9 @@ class Size(var rows: Int, var cols: Int) {
 
 abstract class DungeonGenerator(var size: Size) {
   var r = new Random()
+  
+  type Matrix[T] = Array[Array[T]]
+  
   def generate: String
 
   def printArray(a: Array[Array[Int]]): String = {
@@ -59,7 +63,9 @@ class Position(var x: Int, var y: Int) {
   def +(delta: (Int, Int)): Position = {
     return new Position(x + delta._1, y + delta._2)
   }
-
+  
+    override def toString: String =
+    "(" + x + ", " + y + ")"
 }
 
 // pimpmylib http://stackoverflow.com/questions/2633719/is-there-an-easy-way-to-convert-a-boolean-to-an-integer
@@ -72,9 +78,11 @@ class Categorical(var probabilities: List[Float]) extends Distribution {
   def draw(): Int = {
     var cumulativeProbabilities = probabilities.scanLeft(0f)(_ + _)
     var p = new Random().nextFloat()
-    return cumulativeProbabilities.indexWhere(_ < p)
+    return cumulativeProbabilities.indexWhere(_ > p)-1
   }
 }
+
+
 
 class SimpleMaze(size: Size, var keepDirectionProbability: Float) extends DungeonGenerator(size) {
 
@@ -83,41 +91,49 @@ class SimpleMaze(size: Size, var keepDirectionProbability: Float) extends Dungeo
   }
   implicit def convertBooleanToInt(b: Boolean) = new asInt(b)
 
+  def possibleMoves(p:Position,visited:Array[Array[Boolean]]):(Seq[Position],Seq[DirectionVal])={
+    
+     val validPositionFactor = posiblePositions.map(p => (size.isValid(p) && !visited(p.x)(p.y)) toInt)
+     
+  }
   def generate(): String = {
 
     //var tiles = Array.ofDim[Int](size.rows, size.cols)
     var visited = Array.ofDim[Boolean](size.rows, size.cols)
-
+    var isWall = Array.ofDim[Boolean](size.rows, size.cols)
+    
     var p = new Position(r.nextInt(size.rows), r.nextInt(size.cols))
     var previousDirection: DirectionVal = Direction.South
     visited(p.x)(p.y) = true;
     var visitedOrWallCount = 1
 
     while (visitedOrWallCount < size.count) {
-
-      val posiblePositions = Direction.values.map(d => p + d.asTuple())
-
-      val validPositionFactor = posiblePositions.map(x => size.isValid(x) toInt)
-
-      val visitedPositionInt = posiblePositions.map(p => visited(p.x)(p.y) toInt)
-      val visitedPositionFactor = visitedPositionInt.map(f => 1-f)
+      println(visitedOrWallCount+ "/"+ size.count + " -> " + p)
+      
+      val (posiblePositions,possibleDirections) = possibleMoves(p,size)
+      println(Direction.values)
+      println(posiblePositions)
+      val validPositionFactor = posiblePositions.map(p => (size.isValid(p) && !visited(p.x)(p.y)) toInt)
 
       val previousDirectionIndex = Direction.values.indexOf(previousDirection)
       val newDirectionProbability = (1 - keepDirectionProbability) / (Direction.values.length - 1)
       var mantainDirectionFactor = List.fill(Direction.values.length)(newDirectionProbability)
       mantainDirectionFactor.updated(previousDirectionIndex, keepDirectionProbability)
 
-      var positionProbabilities = (validPositionFactor, mantainDirectionFactor, visitedPositionFactor).zipped.map(_ * _ * _)
+      var positionProbabilities = (validPositionFactor, mantainDirectionFactor).zipped.map(_ * _)
       val totalProbability = positionProbabilities.sum
-
+      
+      println(positionProbabilities)
+      
       if (totalProbability > 1e-9) {
         positionProbabilities = positionProbabilities.map(_ / totalProbability)
         val positionsDistribution = new Categorical(positionProbabilities.toList)
         val newPositionIndex = positionsDistribution.draw()
+        println(newPositionIndex)  
         previousDirection = Direction.values(newPositionIndex)
         p = posiblePositions(newPositionIndex)
       } else { // deadend, look somewhere else
-
+        visitedOrWallCount =size.count
       }
 
       visited(p.x)(p.y) = true;
